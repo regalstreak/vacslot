f4201e95cdf96a92002c16422dc87a7ebc4e5f78
Neil Agarwal <neil@neilagarwal.me>
2021-05-04 17:12:55 +0530

    feat: increase codepush sync freq

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ import { name as appName } from './app.json';
 import { LogBox } from 'react-native';
 import { registerBackgroundFetchHeadlessTasks } from 'src/utils/backgroundUpdater';
 import { initialisePushNotifications } from 'src/utils/notifications';
+import CodePush from 'react-native-code-push';
 
 AppRegistry.registerComponent(appName, () => App);
 
 initialisePushNotifications();
 registerBackgroundFetchHeadlessTasks();
+
+CodePush.sync({
+    checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+    installMode: CodePush.InstallMode.ON_NEXT_RESUME,
+    updateDialog: false,
+});
 
 LogBox.ignoreLogs(['Setting a timer']);
