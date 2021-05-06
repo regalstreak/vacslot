977988b3ea1aa8b732ac2668149d1c8501682bfe
Neil Agarwal <neil@neilagarwal.me>
2021-05-06 16:43:15 +0530

    fix: api userAgent header

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
+import randomUserAgent from 'random-useragent';
+
 export const API_BASE_URL = 'https://cdn-api.co-vin.in/api/v2';
 
 export const API_ENDPOINTS = {
@@ -9,7 +11,19 @@ export const API_ENDPOINTS = {
         '/appointment/sessions/public/calendarByDistrict/',
 };
 
+const userAgent = randomUserAgent.getRandom((ua) => {
+    return (
+        ua.browserName === 'Chrome' ||
+        ua.browserName === 'Firefox' ||
+        ua.browserName === 'Edge' ||
+        ua.browserName === 'Safari'
+    );
+});
+
+console.log(userAgent);
+
 export const API_HEADERS = {
     Accept: 'application/json',
     'Accept-Language': 'en_IN',
+    'User-Agent': userAgent,
 };
